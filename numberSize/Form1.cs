e5fbdc418c025a46837904650b5df86adafc93c9
pogi0 <pogi0@DEX_DOMINGO>
2023-09-10 19:46:16 +0800

    Add project files.

---
+++ numberSize/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberSize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        _sizes size = new _sizes();
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+                size.setSize(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                textBox3.Text = ("" + size.getSize());
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                MessageBox.Show("" + size.getSize() + size.getValue());
+            
+
+           
+        }
+    }
+}
---
+++ numberSize/_sizes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberSize
+{
+    internal class _sizes
+    {
+        private int size;
+        private string value;
+
+        public void setSize(int firstNum, int secondNum)
+        {
+            size = firstNum + secondNum;
+
+
+        }
+        public int getSize()
+        {
+
+            return size;
+        }
+
+
+
+        public string getValue()
+        {
+            int num = 0;
+            int num2 = 11;
+            int num3 = 17;
+            int num4 = 21; ;
+
+            if (num <= size && num >= 10)
+            {
+                value = "small";
+                return value;
+
+            }
+            else if (num2 <= size && size <= 16)
+            {
+                value = "med";
+                return value;
+
+            }
+            else if (num3 <= size && size <= 31)
+            {
+
+                value = " large";
+                return value;
+
+            }
+            else if (num4 <= size && size >= 32)
+            {
+                value = "xl";
+                return value;
+            }
+            else 
+            {
+                value = "error";
+                return value;
+
+            }
+            
+        }
+    }
+
+
+    }
+
